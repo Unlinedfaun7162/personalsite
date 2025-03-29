6563c378365fe942d46ff995d098b2d6549495b5
Unlinedfaun7162 <biswashlamsal228@gmail.com>
2025-03-29 00:09:32 -0400

    Check database

--- src/pages/GroceryStore.js
+++ src/pages/GroceryStore.js
@@ -52,9 +52,8 @@ const GroceryStore = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {Array.from({ length: 8 }).map((_, index) => (
           <img
-            key={index}
-            src={`${process.env.PUBLIC_URL}/database${index + 1}.png`}
-            alt={`Database Design ${index + 1}`}
+            src={`${process.env.PUBLIC_URL}/Database1.png`}
+            alt="Database Design 1"
             className="rounded-lg shadow-md w-full h-auto object-contain border"
           />
         ))}
